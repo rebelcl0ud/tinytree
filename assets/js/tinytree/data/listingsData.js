a4208887a92fb5ea96a8d86aad2283537f356350
rebelcl0ud <jovanka.placeres@gmail.com>
2019-01-22 15:51:13 -0800

    add: dummy data file for testing

---
+++ assets/js/tinytree/data/listingsData.js
@@ -0,0 +1,72 @@
+var listingsData = [
+	{
+		address: '123 Place',
+		city: 'City Place',
+		state: 'Tennessee',
+		locationName: '',
+		rooms: 2,
+		status: 'buy',
+		type: 'tiny house',
+		price: 25000,
+		area: 800,
+		features: ['parking', 'pet-friendly', 'parking'],
+		image:
+			'http://tinyhousepins.com/wp-content/uploads/2013/08/might-micro-house-on-wheels-for-sale-01-600x450.jpg'
+	},
+	{
+		address: '123 Pointe Place',
+		city: 'Pointe Place',
+		state: 'Wisconsin',
+		locationName: '',
+		rooms: 1,
+		status: 'rent',
+		type: 'tiny house',
+		price: 15000,
+		area: 1000,
+		features: ['parking', 'pet-friendly', 'parking'],
+		image:
+			'http://tinyhomesforsale.weebly.com/uploads/4/1/0/3/41030213/7617293.jpg?297'
+	},
+	{
+		address: '786 Pointe Blvd',
+		city: 'Place City',
+		state: 'Wisconsin',
+		locationName: '',
+		rooms: 2,
+		status: 'buy',
+		type: 'tiny house',
+		price: 5000,
+		area: 1800,
+		features: ['parking', 'pet-friendly', 'parking'],
+		image: 'http://www.simplesolarhomes.com/images/cabin.jpg'
+	},
+	{
+		address: '987 Who Dr',
+		city: 'Wat City',
+		state: 'Wyoming',
+		locationName: '',
+		rooms: 1,
+		status: 'buy',
+		type: 'tiny house',
+		price: 15000,
+		area: 1800,
+		features: ['parking', 'pet-friendly', 'parking'],
+		image:
+			'http://colorcountrypainting.com/wp-content/uploads/2010/08/DSCo4818smalll.jpg'
+	},
+	{
+		address: '777 Gold Rd',
+		city: 'Emerald City',
+		state: 'Kansas',
+		locationName: '',
+		rooms: 3,
+		status: 'buy',
+		type: 'tiny house',
+		price: 45000,
+		area: 2800,
+		features: ['parking', 'pet-friendly', 'parking'],
+		image: 'https://i.ytimg.com/vi/td9XIHdujwI/hqdefault.jpg'
+	}
+];
+
+export default listingsData;
