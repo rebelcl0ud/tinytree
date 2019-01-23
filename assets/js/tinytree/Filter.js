e803d2e085a831305efb05c8d1643c6670748381
rebelcl0ud <jovanka.placeres@gmail.com>
2019-01-23 15:52:11 -0800

    add: onChange prop added to all inputs, selections; values put in; names used in state changed to have underscores rather than dashes; more options added

--- assets/js/tinytree/Filter.js
+++ assets/js/tinytree/Filter.js
@@ -9,23 +9,58 @@ export default class Filter extends Component {
 			<section id="filter">
 				<div className="insideFilter">
 					<h2>FILTER</h2>
-					<select name="location" className="filters location">
-						<option>Tennessee</option>
+					<select
+						name="location"
+						className="filters location"
+						onChange={this.props.onChange}
+					>
+						<option value="Tennessee">Tennessee</option>
+						<option value="Kentucky">Kentucky</option>
+						<option value="Montana">Montana</option>
 					</select>
-					<select name="rooms" className="filters rooms">
-						<option>2BR</option>
+					<select
+						name="rooms"
+						className="filters rooms"
+						onChange={this.props.onChange}
+					>
+						<option value="0">0BR</option>
+						<option value="1">1BR</option>
+						<option value="2">2BR</option>
+						<option value="3">3BR+</option>
 					</select>
-					<select name="status" className="filters status">
-						<option>Rent</option>
+					<select
+						name="status"
+						className="filters status"
+						onChange={this.props.onChange}
+					>
+						<option value="vacation">vacation</option>
+						<option value="rent">rent</option>
+						<option value="buy">buy</option>
 					</select>
-					<select name="type" className="filters type">
+					<select
+						name="type"
+						className="filters type"
+						onChange={this.props.onChange}
+					>
 						<option>treehouse</option>
 						<option>tinyhouse</option>
 					</select>
 					<div className="filters price">
 						<span className="title">price</span>
-						<input type="text" name="min-price" className="min-price" />
-						<input type="text" name="max-price" className="max-price" />
+						<input
+							type="text"
+							name="min_price"
+							className="min-price"
+							onChange={this.props.onChange}
+							value={this.props.globalState.min_price}
+						/>
+						<input
+							type="text"
+							name="max_price"
+							className="max-price"
+							onChange={this.props.onChange}
+							value={this.props.globalState.max_price}
+						/>
 					</div>
 					<div className="filters features">
 						<span className="title">features</span>
@@ -36,15 +71,17 @@ export default class Filter extends Component {
 								value="accessible"
 								name="accessible"
 								className="accessible"
+								onChange={this.props.onChange}
 							/>
 						</label>
 						<label htmlFor="pet-friendly">
 							pet-friendly
 							<input
 								type="checkbox"
-								value="pet-friendly"
-								name="pet-friendly"
+								value="pet_friendly"
+								name="pet_friendly"
 								className="pet-friendly"
+								onChange={this.props.onChange}
 							/>
 						</label>
 						<label htmlFor="parking">
@@ -54,6 +91,7 @@ export default class Filter extends Component {
 								value="parking"
 								name="parking"
 								className="parking"
+								onChange={this.props.onChange}
 							/>
 						</label>
 					</div>
